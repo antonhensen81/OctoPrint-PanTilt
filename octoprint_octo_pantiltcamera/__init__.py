137a5f732ecb549f35f51e13cf2000c45bd62ca7
Anton Hensen <antonhensen@gmail.com>
2020-03-07 14:33:57 +0100

    initial commit

---
+++ octoprint_octo_pantiltcamera/__init__.py
@@ -0,0 +1,155 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+import sarge
+import flask
+from time import sleep
+import RPi.GPIO as GPIO
+
+class Octo_pantiltcameraPlugin(octoprint.plugin.SettingsPlugin,
+                    octoprint.plugin.AssetPlugin,
+                    octoprint.plugin.TemplatePlugin,
+                    octoprint.plugin.SimpleApiPlugin,
+                    octoprint.plugin.StartupPlugin):
+
+	def __init__(self):
+		self.panValue = 0
+		self.tiltValue = 0
+
+	def on_after_startup(self):
+		self.panValue = int(self._settings.get(["pan", "initialValue"]))
+		self.tiltValue = int(self._settings.get(["tilt", "initialValue"]))
+		self.panPort = int(self._settings.get(["pan", "gpio"]))
+		self.tiltPort = int(self._settings.get(["tilt", "gpio"]))
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.panPort, GPIO.OUT)
+		GPIO.setup(self.tiltPort, GPIO.OUT)
+
+		self.setAngles(self.panValue, self.tiltValue)
+
+	def get_template_configs(self):
+		return [
+		    dict(type="settings", custom_bindings=False)
+		]
+
+	##~~ SettingsPlugin mixin
+	def get_settings_defaults(self):
+		return dict(
+			pan=dict(
+				stepSize=5,
+				initialValue=30,
+				minValue=-10,
+				maxValue=60,
+				gpio=17,
+				invert=True
+			),
+			tilt=dict(
+				stepSize=5,
+				initialValue=95,
+				minValue=50,
+				maxValue=120,
+				gpio=18,
+				invert=False,
+			),
+		)
+
+	def setServoAngle(self, servo, angle):
+		pwm = GPIO.PWM(servo, 50)
+		pwm.start(8)
+		dutyCycle = angle / 18. + 3.
+		pwm.ChangeDutyCycle(dutyCycle)
+		sleep(0.3)
+		pwm.stop()
+
+	def setAngles(self, panValue, tiltValue):
+ 		self._logger.info("pan: {}".format(self.panValue))
+		self._logger.info("tilt: {}".format(self.tiltValue))
+		self.setServoAngle(self.panPort, panValue)
+		self.setServoAngle(self.tiltPort, tiltValue)
+
+	def get_assets(self):
+		return dict(
+			js=["js/octo_pantiltcamera.js"],
+			css=["css/octo_pantiltcamera.css"],
+			less=["less/octo_pantiltcamera.less"]
+		)
+
+	def get_api_commands(self):
+		return dict(
+			set =[],
+			left=[],
+			right=[],
+			up=[],
+			down=[]
+		)
+
+	def on_api_command(self, command, data):
+		if command == "set":
+			if "panValue" in data:
+				panValue = int(data["panValue"])
+			if "tiltValue" in data:
+				tiltValue = int(data["tiltValue"])
+			self.setAngles(panValue, tiltValue)
+			self.panValue = panValue
+			self.tiltValue = tiltValue
+		elif command == "left" or command == "right":
+			panValue = int(self.panValue)
+
+			stepSize = int(self._settings.get(["pan", "stepSize"]))
+			if stepSize in data:
+				stepSize = int(data["stepSize"])
+
+			if self._settings.get(["pan", "invert"]):
+				panValue = panValue - (stepSize if command == "right" else -stepSize)
+			else:
+				panValue = panValue + (stepSize if command == "right" else -stepSize)
+
+			self.setAngles(panValue, self.tiltValue)
+			self.panValue = panValue
+		elif command == "up" or command == "down":
+			tiltValue = int(self.tiltValue)
+
+			stepSize = int(self._settings.get(["tilt", "stepSize"]))
+			if stepSize in data:
+				stepSize = int(data["stepSize"])
+
+			if self._settings.get(["tilt", "invert"]):
+				tiltValue = tiltValue - (stepSize if command == "up" else -stepSize)
+			else:
+				tiltValue = tiltValue + (stepSize if command == "up" else -stepSize)
+
+			self.setAngles(self.panValue, tiltValue)
+			self.tiltValue = tiltValue
+
+	def on_api_get(self, request):
+		return flask.jsonify(panValue=self.panValue, tiltValue=self.tiltValue)
+
+	def get_update_information(self):
+		return dict(
+			octo_pantiltcamera=dict(
+				displayName="Pan/Tilt Plugin",
+				displayVersion=self._plugin_version,
+				type="github_release",
+				user="antonhensen81",
+				repo="OctoPrint-PanTilt",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/antonhensen81/OctoPrint-PanTilt/archive/{target_version}.zip"
+			)
+		)
+
+
+__plugin_name__ = "OctoPrint Pan Tilt Camera"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = Octo_pantiltcameraPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
---
+++ octoprint_octo_pantiltcamera/static/js/octo_pantiltcamera.js
@@ -0,0 +1,76 @@
+$(function() {
+    function PantiltViewModel(parameters) {
+        var self = this;
+
+        self.loginState = parameters[0];
+
+        self.stepSize = ko.observable(1);
+
+        self.panValue = ko.observable(0);
+        self.tiltValue = ko.observable(0);
+
+        self.requestData = function() {
+            OctoPrint.simpleApiGet("octo_pantiltcamera").done(function(response) {
+                self.panValue(response.panValue);
+                self.tiltValue(response.tiltValue);
+            });
+        };
+
+        self.onStartup = function() {
+            var webcam = $("#webcam_container");
+            if (webcam === undefined)
+                return;
+
+            webcam.after(self.createPanel());
+            self.requestData();
+        };
+
+        self.createPanel = function() {
+            var panel = $("<!-- ko allowBindings: false --><div id='control_pantilt'></div><!-- /ko -->");
+            panel.append($("<h1>Webcam Pan/Tilt</h1>"));
+
+            var buttons = $("<div style='margin-right: 20px'></div>");
+            buttons.append(self.createButton("left"));
+            buttons.append(self.createButton("right"));
+            buttons.append(self.createButton("up"));
+            buttons.append(self.createButton("down"));
+            panel.append(buttons);
+
+            var values = $("<div></div>");
+            values.append(self.createInput("Pan", "panValue"));
+            values.append(self.createInput("Tilt", "tiltValue"));
+            values.append($("<button id='control-pantilt-set' class='btn pull-left' data-bind=\"loginState.isUser(), click: function() { $root.sendSetCommand(); }\">Set</button>"));
+            panel.append(values);
+
+            return panel;
+        };
+
+        self.createButton = function(command) {
+            var button = $("<button id='control-pantilt-" + command + "' class='btn box pull-left' data-bind=\"loginState.isUser(), click: function() { $root.sendCommand('" + command + "'); }\"></button>");
+            button.append($("<i class='icon-arrow-" + command + "'></i>"));
+            return button;
+        };
+
+        self.createInput = function(name, variable) {
+            var div = $("<div class='pull-left' style='margin-right: 10px'></div>");
+            div.append(document.createTextNode(name + ": "));
+            div.append($("<input type='number' style='width: 60px' data-bind='value: "+ variable +"'>"));
+
+            return div;
+        };
+
+        self.sendSetCommand = function() {
+            OctoPrint.simpleApiCommand("octo_pantiltcamera", "set", {panValue: self.panValue(), tiltValue: self.tiltValue()}).done(self.requestData);
+        };
+        self.sendCommand = function(command) {
+            OctoPrint.simpleApiCommand("octo_pantiltcamera", command, {stepSize: self.stepSize()}).done(self.requestData);
+        };
+    }
+
+    // view model class, parameters for constructor, container to bind to
+    OCTOPRINT_VIEWMODELS.push([
+        PantiltViewModel,
+        ["loginStateViewModel"],
+        ["#control_pantilt"]
+    ]);
+});
